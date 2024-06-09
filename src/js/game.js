37080710291f6cf4d1ee7d6b21dceb4afd1ad9dc
ijwgt <66661966@bk.ru>
2024-06-09 15:54:15 +0300

    task1

---
+++ src/js/game.js
@@ -0,0 +1,24 @@
+function orderByProps(obj, sortOrder) {
+	const sortedArr = [];
+	const abcSortedArr = [];
+
+	for (const key in obj) {
+		const newObj = { key: key, value: obj[key] };
+
+		if(sortOrder.includes(key)) {
+			sortedArr.push(newObj);
+		} else{
+			abcSortedArr.push(newObj);
+		}
+	}
+
+	abcSortedArr.sort((a, b) => {
+		if (a.key > b.key) {
+			return 1;
+		}
+		return -1;
+	});
+	
+	const arr = [...sortedArr, ...abcSortedArr];
+	return arr;
+}
